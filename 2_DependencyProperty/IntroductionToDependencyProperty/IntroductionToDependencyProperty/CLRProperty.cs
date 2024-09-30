5ace9173a10a3553bb7dc5a4c3839685bddec468
zhaotianff <zhaotianff@163.com>
2024-09-30 17:38:18 +0800

    add dependency property demo

---
+++ 2_DependencyProperty/IntroductionToDependencyProperty/IntroductionToDependencyProperty/CLRProperty.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntroductionToDependencyProperty
+{
+    public class CLRProperty
+    {
+        private int id;
+
+        public int Id
+        {
+            get => id;
+            set => id = value;
+        }
+    }
+}
---
+++ 2_DependencyProperty/IntroductionToDependencyProperty/IntroductionToDependencyProperty/MyButton.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace IntroductionToDependencyProperty
+{
+    public class MyButton : Button
+    {
+        public static readonly DependencyProperty ImageProperty;
+
+
+        static MyButton()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(MyButton), new FrameworkPropertyMetadata(typeof(MyButton)));
+
+            ImageProperty = DependencyProperty.Register("Image", typeof(ImageSource), typeof(MyButton),new PropertyMetadata(null, OnImagePropertyChanged));
+        }
+
+
+        /// <summary>
+        /// 当ImageProperty依赖属性值更改时会被调用
+        /// </summary>
+        /// <param name="d"></param>
+        /// <param name="e"></param>
+        private static void OnImagePropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+
+        }
+
+        /// <summary>
+        /// 属性包装器
+        /// </summary>
+        public ImageSource Image
+        {
+            get => (ImageSource)GetValue(ImageProperty);
+            set => SetValue(ImageProperty, value);
+        }
+    }
+}
