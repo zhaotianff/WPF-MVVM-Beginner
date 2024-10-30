42dc4e69ddac73a26dba86c18fdc8f8e06e73508
zhaotianff <zhaotianff@163.com>
2024-10-30 17:03:59 +0800

    add basic datacontext demo

---
+++ 3_DataBinding/DataBinding/BasicDataContextDemo/MyData.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicDataContextDemo
+{
+    public class MyData
+    {
+        private string displayText = "HelloWorld";
+
+        public string DisplayText
+        {
+            get => displayText;
+            set => displayText = value;
+        }
+    }
+}
