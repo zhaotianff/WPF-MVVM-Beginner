6d0bd5dc2035167e32e18f5398de71fd40288114
zhaotianff <zhaotianff@163.com>
2024-11-07 17:34:20 +0800

    update data binding demo

---
+++ 3_DataBinding/DataBinding/PropertyChangeNotifyDemo/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PropertyChangeNotifyDemo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            var myData = new MyData();
+            this.textbox.DataContext = myData;
+
+            var myData2 = new MyData2();
+            this.textbox2.DataContext = myData2;
+        }
+
+
+        private void btn_Update_Click(object sender, RoutedEventArgs e)
+        {
+            var myData = this.textbox.DataContext as MyData;
+            myData.DisplayText = "tragedy";  //ui not update
+        }
+
+
+        private void btn_Read_Click(object sender, RoutedEventArgs e)
+        {
+            //after edit textbox
+            //property update 
+            var myData = this.textbox.DataContext as MyData;
+            MessageBox.Show(myData.DisplayText);
+        }
+
+        private void btn_Read2_Click(object sender, RoutedEventArgs e)
+        {
+            //after edit textbox
+            //property update
+            var myData2 = this.textbox2.DataContext as MyData2;
+            MessageBox.Show(myData2.DisplayText2);
+        }
+
+        private void btn_Update2_Click(object sender, RoutedEventArgs e)
+        {
+            var myData2 = this.textbox2.DataContext as MyData2;
+            myData2.DisplayText2 = "tragedy";  //ui update
+        }
+    }
+}
---
+++ 3_DataBinding/DataBinding/PropertyChangeNotifyDemo/MyData.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PropertyChangeNotifyDemo
+{
+    public class MyData
+    {
+        private string displayText = "HelloWorld";
+
+        public string DisplayText
+        {
+            get => displayText;
+            set => displayText = value;
+        }
+    }
+
+    public class MyData2 : INotifyPropertyChanged
+    {
+        private string displayText2 = "HelloWorld";
+
+        public string DisplayText2
+        {
+            get => displayText2;
+            set
+            {
+                displayText2 = value;
+
+                PropertyChanged?.Invoke(this,new PropertyChangedEventArgs("DisplayText2"));
+
+                //or
+                //OnPropertyChanged();
+            }
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+
+        protected void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
