c680496ff8cef6ad9ef191b57871319d1b09aa47
zhaotianff <zhaotianff@163.com>
2024-11-19 17:34:36 +0800

    update data binding demo

---
+++ 4_DataBindingDemo/DataBindingDemo/1_ComplexDataObject/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace _1_ComplexDataObject
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
---
+++ 4_DataBindingDemo/DataBindingDemo/1_ComplexDataObject/MainWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _1_ComplexDataObject
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : TianXiaTech.BlurWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void LoadBook()
+        {
+            ObservableBook observableBook = new ObservableBook();
+            observableBook.CoverImageUrl = "http://img3m1.ddimg.cn/16/5/29681701-1_w_1709623057.jpg";
+            this.DataContext = observableBook;
+        }
+    }
+}
