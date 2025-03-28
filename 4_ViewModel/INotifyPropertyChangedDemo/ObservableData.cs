5ab9b01d41b0f90ec66a3dd040b002623eec534f
zhaotianff <zhaotianff@163.com>
2025-03-28 17:17:15 +0800

    update viewmodel demo

---
+++ 4_ViewModel/INotifyPropertyChangedDemo/ObservableData.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace INotifyPropertyChangedDemo
+{
+    public class ObservableData : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        private string currentTime;
+
+        public string CurrentTime
+        {
+            get => currentTime;
+            set
+            {
+                currentTime = value;
+
+                RaiseChanged();
+            }
+        }
+
+        private void RaiseChanged([CallerMemberName] string memeberName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(memeberName));
+        }
+    }
+}
