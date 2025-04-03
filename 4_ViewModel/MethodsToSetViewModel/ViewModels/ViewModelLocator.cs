99fd618f7ca2c06e63b281cba78af56e763785a8
zhaotianff <zhaotianff@163.com>
2025-04-03 17:28:31 +0800

    update viewmodel demo

---
+++ 4_ViewModel/MethodsToSetViewModel/ViewModels/ViewModelLocator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsToSetViewModel.ViewModels
+{
+    public class ViewModelLocator
+    {
+        private MainWindowViewModel mainWindowViewModel;
+
+        public MainWindowViewModel MainWindowViewModel
+        {
+            get
+            {
+                if (mainWindowViewModel == null)
+                    mainWindowViewModel = new MainWindowViewModel();
+
+                return mainWindowViewModel;
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # WPF-MVVM-Beginner
 Tutorials for WPF MVVM beginner
+
+# List
+* [MVVMPattern](1_MVVMPattern)
+* [DependencyProperty](2_DependencyProperty)
+* [DataBinding](3_DataBinding)
+* [ViewModel](4_ViewModel)
+* [CommandAndUserInput](5_CommandAndUserInput)
+* [ViewModelDemo](6_ViewModelDemo)
