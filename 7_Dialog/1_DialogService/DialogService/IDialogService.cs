f4dae715507e6705a0a214fa65843bf23b7f4426
zhaotianff <zhaotianff@163.com>
2025-04-09 17:30:13 +0800

    add dialog demo

---
+++ 7_Dialog/1_DialogService/DialogService/IDialogService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace _1_DialogService.DialogService
+{
+    public interface IDialogService
+    {
+        MessageBoxResult ShowMessage(string title, string content);
+    }
+}
