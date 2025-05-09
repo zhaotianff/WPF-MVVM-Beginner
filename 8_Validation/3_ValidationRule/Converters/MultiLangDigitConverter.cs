3008b05b3efe1c9d6f3997cb2edbc5f76143f814
zhaotianff <zhaotianff@163.com>
2025-05-09 14:49:56 +0800

    update custom validation rule demo

---
+++ 8_Validation/3_ValidationRule/Converters/MultiLangDigitConverter.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace _3_ValidationRule.Converters
+{
+    public class MultiLangDigitConverter : IValueConverter
+    {
+        /// <summary>
+        /// 从源到目标
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="targetType"></param>
+        /// <param name="parameter"></param>
+        /// <param name="culture"></param>
+        /// <returns></returns>
+        /// <exception cref="NotImplementedException"></exception>
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value;
+        }
+
+        /// <summary>
+        /// 从目标到源
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="targetType"></param>
+        /// <param name="parameter"></param>
+        /// <param name="culture"></param>
+        /// <returns></returns>
+        /// <exception cref="NotImplementedException"></exception>
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var val = value.ToString();
+
+            //直接输入了数字
+            if (int.TryParse(val, out int numValue))
+            {
+                return numValue;
+            }
+            else
+            {
+                var res = Application.Current.TryFindResource(val);
+
+                if(res != null)
+                {
+                    return res;
+                }
+
+                return value;
+            }
+        }
+    }
+}
--- 8_Validation/3_ValidationRule/ViewModels/MainWindowViewModel.cs
+++ 8_Validation/3_ValidationRule/ViewModels/MainWindowViewModel.cs
@@ -1,15 +1,22 @@
-﻿using System;
+﻿using _3_ValidationRule.Commands;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
 
 namespace _3_ValidationRule.ViewModels
 {
     public class MainWindowViewModel : INotifyPropertyChanged
     {
         public event PropertyChangedEventHandler? PropertyChanged;
+
+        public ICommand SwitchChsCommand { get; private set; }
+
+        public ICommand SwitchEnCommand { get; private set; }
 
         private int rangeDigitValue;
 
@@ -21,6 +28,37 @@ namespace _3_ValidationRule.ViewModels
                 rangeDigitValue = value;
                 PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("RangeDigitValue"));
             }
+        }
+
+        private int rangeDigitValueWithConverter = 1;
+
+        public int RangeDigitValueWithConverter
+        {
+            get => rangeDigitValueWithConverter;
+            set
+            {
+                rangeDigitValueWithConverter = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("RangeDigitValueWithConverter"));
+            }
+        }
+
+        public MainWindowViewModel()
+        {
+            SwitchChsCommand = new RelayCommand(SwtichToChs);
+            SwitchEnCommand = new RelayCommand(SwitchToEn);
+        }
+
+
+        public void SwtichToChs()
+        {
+            //中文
+            Application.Current.Resources.MergedDictionaries[0] = new ResourceDictionary() { Source = new Uri("Lang/zh-CN.xaml", UriKind.Relative) };
+        }
+
+        public void SwitchToEn()
+        {
+            //英文
+            Application.Current.Resources.MergedDictionaries[0] = new ResourceDictionary() { Source = new Uri("Lang/en-US.xaml", UriKind.Relative) };
         }
     }
 }
