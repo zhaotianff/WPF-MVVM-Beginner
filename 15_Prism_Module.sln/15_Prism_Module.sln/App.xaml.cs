cb4c3a3f13bf559bfdb9be87c0a0ab1f2d404ee3
zhaotianff <zhaotianff@163.com>
2026-02-03 17:58:43 +0800

    add Prism Module demo

---
+++ 15_Prism_Module.sln/15_Prism_Module.sln/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace _15_Prism_Module.sln
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
---
+++ 15_Prism_Module.sln/Module.Register/RegisterModule.cs
@@ -0,0 +1,18 @@
+﻿
+using Prism.Ioc;
+
+namespace Module.Register
+{
+    public class RegisterModule : Prism.Modularity.IModule
+    {
+        public void OnInitialized(IContainerProvider containerProvider)
+        {
+            
+        }
+
+        public void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            
+        }
+    }
+}
